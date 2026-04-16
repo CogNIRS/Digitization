344b4e17a8fc32561b41d80a85fa9b4393a6ecdd
jcjiazhang <148927956+jcjiazhang@users.noreply.github.com>
2026-04-16 17:02:39 +0100

    update

---
+++ SCItoMNI/SCStoMNI.m
@@ -0,0 +1,8 @@
+% Digitizer file names
+F{1,1}= "C:\#RMAPHBT\SCItoMNI\Data\S3\capS_p06_MotCorr_coord.csv";
+F{1,2}= "C:\#RMAPHBT\SCItoMNI\Data\S3\capS_p06_MotCorr_orig.csv";
+F{1,3}= "C:\#RMAPHBT\SCItoMNI\Data\channel_config.txt";
+
+
+ %% Spatial Registration %%
+spm_fnirs_spatialpreproc_ui_nofnirs(F)
